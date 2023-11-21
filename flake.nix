b0672f27b6385fd06e5d1fd688a88e856a6e3c5b
Mathias Sven <mathiassven2@hotmail.com>
2023-11-21 04:50:32 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs.nixfzf.url = "github:MathiasSven/nixfzf/nix";
+
+  outputs = { self, nixpkgs, nixfzf }: 
+  let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+  in
+  {
+    packages = forAllSystems (system: {
+      default = self.packages.${system}.nix-out-explorer;
+
+      nix-out-explorer = 
+      let 
+        inherit (pkgs.${system}) makeWrapper lib stdenv;
+      in stdenv.mkDerivation (finalAttrs: {
+        pname = "nix-out-explorer";
+        version = "0.1.0";
+
+        src = ./.;
+
+        nativeBuildInputs = [ makeWrapper ];
+
+        buildInputs = with pkgs.${system}; [
+          nix
+          curl
+          fzf
+          nixfzf.packages.${system}.default
+        ];
+
+        dontBuild = true;
+
+        installPhase = ''
+          install -D nix-out-explorer.sh $out/bin/${finalAttrs.pname}
+
+          wrapProgram $out/bin/${finalAttrs.pname} \
+            --prefix PATH : '${lib.makeBinPath finalAttrs.buildInputs}'
+        '';
+      });
+    });
+  };
+}
---
+++ nix-out-explorer.sh
@@ -0,0 +1,42 @@
+systems="$(nix eval --raw --impure --expr 'builtins.currentSystem')"
+
+systems+=("x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" \
+      "armv6l-linux" "armv7l-linux" "i686-linux" "mipsel-linux" \
+      "armv5tel-linux" "powerpc64le-linux" "riscv64-linux")
+
+baseUrl="https://cache.nixos.org/"
+
+finalDir=$(mktemp -d)
+
+getSystems() {
+  IFS=$'\n'
+  echo "${systems[*]}" | uniq | fzf -q "" -e -m \
+    --layout=reverse \
+    --marker='>>'
+}
+
+fetch() {
+  export NIXPKGS_ALLOW_BROKEN=1
+  nixOutPath=$(nix eval nixpkgs-stable#$2.outPath --system $1 --raw --impure)
+  outPathHask=$(echo "$nixOutPath" | cut -d '/' -f4 | cut -d '-' -f1)
+  narUrlField=$(curl "$baseUrl$outPathHask.narinfo" | grep URL)
+  narUrl="${narUrlField#URL: }"
+
+  mkdir "$finalDir/$1"
+  curl "$baseUrl$narUrl" | xz -dc | nix-store --restore "$finalDir/$1/$2-out/"
+}
+
+mapfile -t selectedSystems < <( getSystems )
+mapfile -t selectedDrv < <( nixfzf --raw )
+
+for system in "${selectedSystems[@]}";
+do 
+  for drv in "${selectedDrv[@]}";
+  do
+    fetch $system $drv
+  done
+done
+
+cd "$finalDir"
+bash
+rm -rf "$finalDir"
